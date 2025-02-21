daaa39121c3bf8c2628cbb764ef2e96b745abe3f
chandra perusomula <chandra.perusomula@gmail.com>
2025-02-22 00:24:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// this is first code in "JS"
+console.log("Solo Leveling");
+
+am=86
+an=60
+console.log(am);
+console.log(an);
+console.log(am + an );
+console.log(am - an );
+console.log(am * an );
+console.log(am % an );
+console.log(am ** an );
+console.log(am / an );
+console.log(am += an );
+console.log(am == an );
+
+// DATA TYPES :- example
+
+var a=84;       // Holding number
+var b="chandra" // Holding String
+
+/* Data Types of JS
+
+1. Primitive Data Type :
+
+    * Number
+example:-
+var a= 10;
+var b=18.95;
+
+    *String
+example:-
+var an='hello'
+var am+"world"
+
+    *Boolean
+    example:-
+    var d=true
+
+
+2. NON-Primitive (reference)Data types :
+      * Object
+      * Array
+      * Regexp
+
+
+*/
+
+
